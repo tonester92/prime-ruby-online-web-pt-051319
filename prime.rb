cbf7d6db4636debabffcdc3074c030626222f097
Antonio Blandin <antonio.blandin25@gmail.com>
2019-06-09 18:30:40 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,2 +1,11 @@
-def prime? (number)
-  
+def prime(n)
+  unless n.is_a? Integer
+  puts "That's not an integer."
+  if n%2 !=0 && n!=2
+    puts "#{n} is prime!"
+  else
+    puts "#{n} is not prime."
+  end
+end
+
+prime(2)
