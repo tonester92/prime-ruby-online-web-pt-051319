5f9dc682aaea840e28c93ee648458426611c5a81
Antonio Blandin <antonio.blandin25@gmail.com>
2019-06-09 18:32:37 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,2 +1,11 @@
 def prime?(number)
-  
+  start = 2
+  if number > 1
+    range = (start..number-1).to_a
+    range.none? do |number_to_test| 
+      number % number_to_test == 0
+    end
+  else
+    false
+  end
+end
